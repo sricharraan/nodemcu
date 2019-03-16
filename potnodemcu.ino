6f94c334fe5046706f471662f95008e950866060
sricharraan <35862917+sricharraan@users.noreply.github.com>
2019-03-16 11:12:16 +0530

    Add files via upload

---
+++ potnodemcu.ino
@@ -0,0 +1,22 @@
+uint8_t Pin = A0;
+ // declare Input pin on NodeMCU Dev Kit
+void setup() {
+pinMode(Pin, INPUT);
+pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+Serial.begin(115200);
+Serial.println(analogRead(Pin));
+if(analogRead(Pin)>=400)
+{
+  digitalWrite(LED_BUILTIN, LOW);
+delay(1000);// Wait for a second
+}
+else
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+delay(1000);// Wait for a second
+}
+
+}
