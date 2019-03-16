6035887568a887db363c3f51605b26379fe881dd
sricharraan <35862917+sricharraan@users.noreply.github.com>
2019-03-16 11:13:15 +0530

    Add files via upload

---
+++ UltraSonicNodeMCU.ino
@@ -0,0 +1,34 @@
+//D0=NC, D1= TRIGGER, D2= ECHO, VCC= 5V
+int long duration ;
+int dist ;
+
+const int trig=D1;
+const int echo=D2;
+
+void setup() 
+{
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+  
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() 
+{
+  digitalWrite(trig,LOW);
+  delay(2);
+  
+  // put your main code here, to run repeatedly:
+  digitalWrite(trig,HIGH);
+  
+  delay(100);
+  digitalWrite(trig,LOW);
+
+  duration=pulseIn(echo,HIGH);
+  dist=duration*0.034/2;
+    //dist=duration/2;
+
+  Serial.print("Distance : ");
+  Serial.println(dist);
+}
